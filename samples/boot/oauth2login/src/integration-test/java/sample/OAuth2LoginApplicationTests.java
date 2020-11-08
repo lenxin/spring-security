9cc4bd14b66afcb0142c9a0df35738342a5eeaa4
lenxin <623685463@qq.com>
2020-11-08 18:30:23 +0800

    update

--- samples/boot/oauth2login/src/integration-test/java/sample/OAuth2LoginApplicationTests.java
+++ samples/boot/oauth2login/src/integration-test/java/sample/OAuth2LoginApplicationTests.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2002-2020 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package sample;
 
 import java.net.URI;
@@ -335,17 +319,17 @@ public class OAuth2LoginApplicationTests {
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http
-				.authorizeRequests((authorizeRequests) -> 
+				.authorizeRequests((authorizeRequests) ->
 					authorizeRequests
 						.anyRequest().authenticated()
 				)
-				.oauth2Login((oauth2Login) -> 
+				.oauth2Login((oauth2Login) ->
 					oauth2Login
-						.tokenEndpoint((tokenEndpoint) -> 
+						.tokenEndpoint((tokenEndpoint) ->
 							tokenEndpoint
 								.accessTokenResponseClient(this.mockAccessTokenResponseClient())
 						)
-						.userInfoEndpoint((userInfoEndpoint) -> 
+						.userInfoEndpoint((userInfoEndpoint) ->
 							userInfoEndpoint
 								.userService(this.mockUserService())
 						)
